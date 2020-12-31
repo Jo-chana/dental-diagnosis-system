6ae8e36967524403a7d032860af6cd319afef023
jochana <c01034784796@gmail.com>
2020-12-31 21:50:27 +0900

    revise model login

--- chikalab/main.py
+++ chikalab/main.py
@@ -32,12 +32,14 @@ def upload_file(name):
         if name == 'occlusion':
             img = get_image_from_request(request)
             file_path, img = get_file_path_with_result_image(img)
+            if file_path is None:
+                flash("얼굴 사진을 보내주세요")
+                return render_template('main/model.html', model_name=name, model_info=model_list[name])
+            else:
+                flash("AI가 치아를 분석하고 있어요! 잠시만 기다려주세요:)")
             brace_result = get_brace_predict(img)
             occ_result = get_occ_predict(img)
             occ_result = round(occ_result, 2)
             results = [f'교정 여부: {brace_result}', f'교정 점수: {occ_result}']
-            if file_path is None:
-                flash("얼굴 사진을 보내주세요")
-                return render_template('main/model.html', model_name=name, model_info=model_list[name])
             return render_template('main/result.html', results=results, img_src=file_path)
         return render_template('main/index.html', model_list=model_list)
