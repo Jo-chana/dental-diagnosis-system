c3b88725ed301d6df6140c512f86e62d3c79f3a4
jochana <c01034784796@gmail.com>
2021-01-11 11:25:38 +0900

    fixed dockerfile typo

--- app/static/scripts/components/main.js
+++ app/static/scripts/components/main.js
@@ -2,10 +2,25 @@ import React from 'react';
 import '../../style.css'
 import {Link} from "react-router-dom";
 
+function navTab(link, title, index) {
+    return (
+        <Link to={'/'.concat(link)}><p className='main-color opacity07-hover' id={'nav'.concat(index.toString())}
+                                       data-id='nav1' onMouseOver={this.mouseOver}
+                                       onMouseLeave={this.mouseLeave} onClick={this.onClick}>{title}</p></Link>
+    )
+}
+
 class Main extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isFirst: true, isClick: '',};
+        this.state = {
+            isFirst: true,
+            isClick: '',
+            tabList: [
+                {link: 'service', title: 'Service'},
+                {link: 'about', title: 'About Us'},
+            ],
+        };
         this.mouseOver = this.mouseOver.bind(this);
         this.mouseLeave = this.mouseLeave.bind(this);
         this.onClick = this.onClick.bind(this);
@@ -18,12 +33,9 @@ class Main extends React.Component {
                     <a href='/'><p className="main-title white text-center">Chika Lab</p></a>
                 </div>
                 <div className='flex-wrap b-white main-tab' id='navTab'>
-                    <Link to='/service'><p className='main-color opacity07-hover' id='nav1'
-                                      data-id='nav1' onMouseOver={this.mouseOver}
-                                      onMouseLeave={this.mouseLeave} onClick={this.onClick}>Service</p></Link>
-                    <Link to='/about'><p className='main-color opacity07-hover' id='nav2'
-                                         data-id='nav2' onMouseOver={this.mouseOver}
-                                         onMouseLeave={this.mouseLeave} onClick={this.onClick}>About Us</p></Link>
+                    {this.state.tabList.map((tab, index) => (
+                        navTab(tab.link, tab.title, index + 1)
+                    ))}
                 </div>
             </div>
         );
